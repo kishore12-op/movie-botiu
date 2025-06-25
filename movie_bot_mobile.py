f0a8ca6f0576c8f36b36107c74cd5823ea294fc2
kishore12-op <kishoreps0471@gmail.com>
2025-06-25 21:54:49 +0530

    Create movie_bot_mobile.py

---
+++ movie_bot_mobile.py
@@ -0,0 +1,6 @@
+git init
+git add movie_bot_mobile.py
+git commit -m "Add Streamlit movie bot"
+git branch -M main
+git remote add origin https://github.com/YOUR_USERNAME/movie-bot.git
+git push -u origin main
